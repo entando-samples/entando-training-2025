f0e72832a6b8ec00a4863af5c07293c934ed7fef
ryanjpburgos <ryan.burgos@entando.com>
2025-05-09 18:43:20 +0200

    minor fixes

--- microservices/expense-management/src/main/java/com/entando/springbootexpense/config/CorsConfig.java
+++ microservices/expense-management/src/main/java/com/entando/springbootexpense/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(allowedApiOrigin.toArray(new String[allowedApiOrigin.size()]))
+                        .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*");
             }
