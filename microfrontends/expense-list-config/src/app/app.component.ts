316cddbaf94c828bddaf9f88ea4a775ab95ad5a3
ryanjpburgos <ryan.burgos@entando.com>
2025-05-13 17:07:46 +0200

    refactor(app.component): improve config handling and formatting
    style(styles.css): comment out Bootstrap import

--- microfrontends/expense-list-config/src/app/app.component.ts
+++ microfrontends/expense-list-config/src/app/app.component.ts
@@ -1,40 +1,61 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'expense-list-config',
   imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrls: ['../styles.css','./app.component.css'],
+  styleUrls: ['../styles.css', './app.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class AppComponent implements OnInit, OnDestroy {
+  _config!: { role: string };
 
-  @Input() config!: { role: string } | string;
+  @Input() set config(value: { role: string } | string) {
+    if (typeof value === 'string') {
+      this._config = JSON.parse(value);
+    } else {
+      this._config = value;
+    }
+    this.configForm.patchValue({
+      role: this._config.role,
+    });
+  }
+
+  get config(): { role: string } {
+    return this._config;
+  }
 
   private subscription!: Subscription;
 
-
   title = 'expense-list-config';
   configForm = new FormGroup({
-    role: new FormControl('', [Validators.required])
-  })
-
+    role: new FormControl('', [Validators.required]),
+  });
 
   ngOnInit(): void {
-    if (typeof this.config === 'string') this.config = JSON.parse(this.config);
-
-    this.subscription = this.configForm.get('role')?.valueChanges.subscribe((value) => {
-      this.config = { role: value as string};
-      console.log('Config changed:', this.config);
-    }) as Subscription;
+    this.subscription = this.configForm
+      .get('role')
+      ?.valueChanges.subscribe((value) => {
+        this.config = { role: value as string };
+      }) as Subscription;
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
-  
-
 }
